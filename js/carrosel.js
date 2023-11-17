447c69940bd0aa8ab86339782c65e0c8b4bf797b
Luiz Gustavo Rodrigues Pereira <137580591+lpereira22@users.noreply.github.com>
2023-11-16 23:40:04 -0300

    Carrosel
    Realizado Carrosel, porem não foi finalizado

---
+++ js/carrosel.js
@@ -0,0 +1,50 @@
+const persona = document.querySelectorAll('.persona');
+const setaEsquerda = document.getElementById('setaEsquerda');
+const setaDireita = document.getElementById('setaDireita');
+
+let currentPersona = 0;
+
+function hidePersona(){
+    persona.forEach(item => item.classList.remove('on'))
+}
+
+function showPersona(){
+    persona[currentPersona].classList.add('on');
+}
+
+function setaDireitaSlider(){
+    hidePersona()
+    if(currentPersona === persona.length -1){
+        currentPersona = 0;
+        }else{
+        currentPersona++;
+    }
+    showPersona();
+}
+
+function setaEsquerdaSlider(){
+    hidePersona()
+    if(currentPersona === 0){
+        currentPersona = persona.length -1;
+    }else{
+        currentPersona--;
+    }
+    showPersona();
+}
+
+setaDireita.addEventListener('click' , setaDireitaSlider);
+setaEsquerda.addEventListener('click', setaEsquerdaSlider);
+
+
+
+setTimeout(function(){
+    hidePersona()
+    if(currentPersona === persona.length -1){
+        currentPersona = 0;
+        }else{
+        currentPersona++;
+    }
+    showPersona();
+}, 2000);
+
+setaDireita.addEventListener(setTimeout);
